382192c2c8bf618ec09add7cbb75e9f8e682620e
athiley.coura <athiley.coura@thoughtworks.com>
2022-07-20 17:12:59 +0200

    set mongo and created first model

---
+++ src/main/kotlin/com/athiley/devlibrary/models/Book.kt
@@ -0,0 +1,13 @@
+package com.athiley.devlibrary.models
+
+import org.springframework.data.annotation.Id
+
+class Books(var name: String?, private var isRead: Boolean?, @Id var isbnId: Number?) {
+
+    override fun toString(): String {
+        return String.format(
+            "com.athiley.devlibrary.models.Customer[id=%s, firstName='%s', lastName='%s']",
+            isbnId, name, isRead
+        )
+    }
+}
