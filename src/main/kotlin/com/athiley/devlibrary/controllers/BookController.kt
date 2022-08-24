b9281570436f13a80a9fd1bc14c6b67e4aea4c53
athiley.coura <athiley.coura@thoughtworks.com>
2022-08-24 14:09:33 +0200

    Integrate sveltekit

--- src/main/kotlin/com/athiley/devlibrary/controllers/BookController.kt
+++ src/main/kotlin/com/athiley/devlibrary/controllers/BookController.kt
@@ -1,9 +1,8 @@
 package com.athiley.devlibrary.controllers
 
 import com.athiley.devlibrary.models.Book
-import com.athiley.devlibrary.repositories.BookRepository
 import com.athiley.devlibrary.services.BookService
-import org.springframework.data.mongodb.repository.Query
+import org.springframework.http.HttpHeaders
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -16,7 +15,13 @@ class BookController(
     @GetMapping("/book")
     fun getReadBooks(@RequestParam("isRead", required=false, defaultValue="false")isRead: Boolean) : ResponseEntity<List<Book>>{
         val books = bookService.fetchBooks(isRead)
-        return ResponseEntity.ok(books)
+        val responseHeaders = HttpHeaders()
+        responseHeaders.set("Access-Control-Allow-Origin",
+            "*");
+
+        return ResponseEntity.ok()
+            .headers(responseHeaders)
+            .body(books)
     }
 }
 
---
+++ svelte/.svelte-kit/ambient.d.ts
@@ -0,0 +1,77 @@
+// this file is generated — do not edit it
+/// <reference types="@sveltejs/kit" />
+
+declare module '$env/static/public' {
+
+}
+
+declare module '$env/static/private' {
+	export const NVM_INC: string;
+	export const TALISMAN_HOME: string;
+	export const NODE: string;
+	export const INIT_CWD: string;
+	export const NVM_CD_FLAGS: string;
+	export const TERM: string;
+	export const SHELL: string;
+	export const npm_config_metrics_registry: string;
+	export const TALISMAN_INTERACTIVE: string;
+	export const TMPDIR: string;
+	export const npm_config_global_prefix: string;
+	export const COLOR: string;
+	export const TERM_SESSION_ID: string;
+	export const npm_config_noproxy: string;
+	export const SDKMAN_PLATFORM: string;
+	export const npm_config_local_prefix: string;
+	export const ZSH: string;
+	export const __INTELLIJ_COMMAND_HISTFILE__: string;
+	export const NVM_DIR: string;
+	export const USER: string;
+	export const COMMAND_MODE: string;
+	export const npm_config_globalconfig: string;
+	export const SDKMAN_CANDIDATES_API: string;
+	export const SSH_AUTH_SOCK: string;
+	export const __CF_USER_TEXT_ENCODING: string;
+	export const npm_execpath: string;
+	export const LOGIN_SHELL: string;
+	export const PAGER: string;
+	export const LOCAL: string;
+	export const LSCOLORS: string;
+	export const PATH: string;
+	export const TERMINAL_EMULATOR: string;
+	export const npm_package_json: string;
+	export const npm_config_engine_strict: string;
+	export const _: string;
+	export const LaunchInstanceID: string;
+	export const npm_config_userconfig: string;
+	export const npm_config_init_module: string;
+	export const __CFBundleIdentifier: string;
+	export const npm_command: string;
+	export const PWD: string;
+	export const JAVA_HOME: string;
+	export const npm_lifecycle_event: string;
+	export const EDITOR: string;
+	export const npm_package_name: string;
+	export const SDKMAN_VERSION: string;
+	export const XPC_FLAGS: string;
+	export const npm_config_node_gyp: string;
+	export const npm_package_version: string;
+	export const XPC_SERVICE_NAME: string;
+	export const GPG_TTY: string;
+	export const SHLVL: string;
+	export const HOME: string;
+	export const npm_config_cache: string;
+	export const LESS: string;
+	export const LOGNAME: string;
+	export const npm_lifecycle_script: string;
+	export const SDKMAN_DIR: string;
+	export const LC_CTYPE: string;
+	export const GITHUB_TOKEN: string;
+	export const NVM_BIN: string;
+	export const npm_config_user_agent: string;
+	export const GITHUB_USERNAME: string;
+	export const OTTO_PROJECT: string;
+	export const SDKMAN_CANDIDATES_DIR: string;
+	export const SECURITYSESSIONID: string;
+	export const npm_node_execpath: string;
+	export const npm_config_prefix: string;
+}
