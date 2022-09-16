40f6e050103bfcc2e416b1be82a84d482cd4d97d
athiley.coura <athiley.coura@thoughtworks.com>
2022-09-16 16:08:58 +0200

    Added the ability to handle errors when status code is not 200

--- svelte/src/routes/book/+page.svelte
+++ svelte/src/routes/book/+page.svelte
@@ -1,18 +1,26 @@
 <script>
     async function response () {
         const response = await fetch('http://localhost:8080/book')
+        if(response.status !== 200){
+            throw new Error()
+        }
         return await response.json()
     }
     const booksPromise = response()
 
+
 </script>
+
 {#await booksPromise then listOfBooks}
     {#each listOfBooks as book}
         <p>{book.name}</p>
         <p>{book.isbnId}</p>
         <p>{book.isRead}</p>
+    {:else}
+        <p>Sorry, you have no books to display :( </p>
     {/each}
-
+{:catch error}
+    <p style="color: red">Sorry, that's sad, something went wrong</p>
 {/await}
 
 
