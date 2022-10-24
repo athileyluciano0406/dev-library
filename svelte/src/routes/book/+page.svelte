decc856b422bd69292c046f1c593137178cf0749
athiley.coura <athiley.coura@thoughtworks.com>
2022-10-24 17:01:38 +0200

    Refactor extracting book element to new component

--- svelte/src/routes/book/+page.svelte
+++ svelte/src/routes/book/+page.svelte
@@ -1,88 +1,14 @@
 <script>
-    async function response () {
-        const response = await fetch('http://localhost:8080/book')
-        if(response.status !== 200){
-            throw new Error()
-        }
-        return await response.json()
-    }
-    const booksPromise = response()
-
-    let title = ''
-    let isbn = ''
-    let result = null
-
-    async function doPost () {
-        const res = await fetch('http://localhost:8080/book', {
-            method: 'POST',
-            body: JSON.stringify({
-                title,
-                isbn
-            }),
-            headers: {"Content-Type": "application/json", Accept: "*/*"}
-        })
-
-        const json = await res.json()
-        result = JSON.stringify(json)
-    }
+    import Book from "../../components/Book.svelte";
 </script>
+
 <div class="library-page">Library</div>
 
-<div class="add-new-book">
-    <h1>Would you like to add a new book to your list?</h1>
-    <input bind:value={title} />
-    <input bind:value={isbn} />
-    <button type="button" on:click={doPost}>
-        Post book.
-    </button>
-<!--    <pre>-->
-<!--        {result}-->
-<!--    </pre>-->
-</div>
-
-
-<div>
-    {#await booksPromise then listOfBooks}
-        <div>
-            {#each listOfBooks as book}
-                <div class="book-element">
-                    <h1>Title</h1>
-                    <div class="book-title">{book.title}</div>
-                    <h2>ISBN</h2>
-                    <div class="book-isbn">{book.isbnId}</div>
-                    <h2>Have you read this book?</h2>
-                    <div class="book-is-read">
-                        {#if book.isRead === true}
-                            <p>Yes</p>
-                            {:else } <p>No</p>
-                        {/if}
-                    </div>
-                </div>
-            {:else}
-                <p>Sorry, you have no books to display :( </p>
-            {/each}
-        </div>
-    {:catch error}
-        <p style="color: red">Sorry, that's sad, something went wrong :( </p>
-    {/await}
-</div>
+<Book/>
 
 <style>
-    .book-element {
-        margin: 20px;
-        padding: 20px;
-        background-color: #d0b5ac;
-        display: inline-block;
-    }
     .library-page {
         margin: 20px;
         font-size: 60px;
     }
-
-    .add-new-book {
-        padding: 20px;
-    }
 </style>
-
-
-
